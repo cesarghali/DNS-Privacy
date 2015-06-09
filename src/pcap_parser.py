59241c1d16d9c9bfd683b11d833744778f1e3659
Christopher Wood <christopherwood07@gmail.com>
2015-06-09 15:27:35 -0700

    Started adding class structure

--- src/pcap_parser.py
+++ src/pcap_parser.py
@@ -1,30 +1,24 @@
 import sys
 import dpkt
 
-counter = 0
-ipcounter = 0
-tcpcounter = 0
-udpcounter = 0
-
 filename = sys.argv[1]
+pcapFile = dpkt.pcap.Reader(open(filename,'r'))
 
-for ts, pkt in dpkt.pcap.Reader(open(filename,'r')):
-  counter += 1
-  eth = dpkt.ethernet.Ethernet(pkt) 
+class Query(object):
+  def __init__(self, ethernetPacket):
+    self.ethernetPacket = ethernetPacket
+    self.unpack()
 
-  if eth.type != dpkt.ethernet.ETH_TYPE_IP:
+  def unpack(self):
+    if eth.type != dpkt.ethernet.ETH_TYPE_IP:
     continue
 
   ip = eth.data
-  ipcounter += 1
+  tcp = ip.data
+    pass
 
-  if ip.p == dpkt.ip.IP_PROTO_TCP: 
-    tcpcounter += 1
+for ts, pkt in pcapFile:
+  eth = dpkt.ethernet.Ethernet(pkt) 
+  query = Query(eth)
 
-  if ip.p == dpkt.ip.IP_PROTO_UDP:
-    udpcounter += 1
-
-print "Total number of packets in the pcap file: ", counter
-print "Total number of ip packets: ", ipcounter
-print "Total number of tcp packets: ", tcpcounter
-print "Total number of udp packets: ", udpcounter
+  # if ip.p == dpkt.ip.IP_PROTO_TCP: 
