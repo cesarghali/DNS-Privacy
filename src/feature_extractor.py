6a3541a4a3e256792c05a78e690aa40c9a2e6618
Christopher Wood <christopherwood07@gmail.com>
2015-06-12 12:38:25 -0700

    Changing spacing

--- src/feature_extractor.py
+++ src/feature_extractor.py
@@ -1,57 +1,84 @@
 import sys
 from pcap_parser import *
 
-class FeatureFormatter(object):
-  def __init__(self, features):
-    self.features = features # list of tuples
+# DONE 1. Relative (per-user) query length
+# 2. Relative source query frequency 
+# 3. Relative target query frequency 
+# 4. Query resolution length (time)
+# 5. Reverse DNS entry IP address ranges
+# 6. Query source-target association (e.g., client/stub-recursive association)
+# 7. Query source identity (address)
+# 8. Query target identity (address)
+# 9. Query diversity (character differences, URI component differences, etc.)
+# 10. Resolution chain length (number of recursive queries)
+# 11. Resolution chain (domains in the chain itself)
+# 12. Monitoring reply from cache (Adv controls in/out links of R and can know if something is served from cache even if the source of the query is anonymised)
 
-  def toCSV(self, stream):
-    for f in self.features:
-      stream.write(",".join(map(lambda x : str(x), f)) + "\n")
+class FeatureFormatter(object):
+    ''' Class that formats lists of features for the output
+    '''
+    def __init__(self, features):
+        self.features = features # list of tuples
+
+    def toCSV(self, stream):
+        for f in self.features:
+            stream.write(",".join(map(lambda x : str(x), f)) + "\n")
 
 class FeatureExtractor(object):
-  def __init__(self, queries):
-    self.queries = queries
+    ''' Base class for all feature extractors.
+    '''
+    def __init__(self, queries):
+        self.queries = queries
 
-  def extract(self):
-    pass
+    def extract(self):
+        pass
 
+class QueryFrequencyFeatureExtractor(FeatureExtractor):
+    def __init__(self, queries):
+        FeatureExtractor.__init__(self, queries)
+
+    def extract(self):
+		features = []
+        sources = []
+		# TODO 
+  
 class QueryLengthFeatureExtractor(FeatureExtractor):
-  def __init__(self, queries):
-    FeatureExtractor.__init__(self, queries)
+    def __init__(self, queries):
+        FeatureExtractor.__init__(self, queries)
 
-  def extract(self):
-    sources = {}
-    features = []
+    def extract(self):
+        sources = {}
+        features = []
 
-    for packet in self.queries:
-      src = None
-      if packet.query != None:
-        src = packet.query.srcAddress
-      if len(packet.records) > 0:
-        src = packet.query.dstAddress
+        for packet in self.queries:
+            src = None
+            if packet.query != None:
+                src = packet.query.srcAddress
+            if len(packet.records) > 0:
+                src = packet.query.dstAddress
 
-      queryLength = len(packet.query.name)
+            queryLength = len(packet.query.name)
 
-      if src not in sources:
-        sources[src] = len(sources) 
-      feature = (sources[src], queryLength)
+            if src not in sources:
+                sources[src] = len(sources) 
+            feature = (sources[src], queryLength)
 
-      features.append(feature)
-    return features
+            features.append(feature)
+        return features
 
 if __name__ == "__main__":
-  if (len(sys.argv) != 2):
-    print >> sys.stderr, "usage: python feature_extractor.py <pcap file>"
-    sys.exit(-1)
+    if (len(sys.argv) != 2):
+        print >> sys.stderr, "usage: python feature_extractor.py <pcap file>"
+        sys.exit(-1)
+        
+    filename = sys.argv[1]
+    print >> sys.stderr, "Parsing...", filename
     
-  filename = sys.argv[1]
-  print >> sys.stderr, "Parsing...", filename
-  
-  dnsPackets = parse(filename)
-  extractor = QueryLengthFeatureExtractor(dnsPackets)
-  features = extractor.extract()
-  formatter = FeatureFormatter(features)
-  print formatter.toCSV(sys.stdout)
+    dnsPackets = parse(filename)
+    extractor = QueryLengthFeatureExtractor(dnsPackets)
+    features = extractor.extract()
+    formatter = FeatureFormatter(features)
+    print formatter.toCSV(sys.stdout)
 
-  print >> sys.stderr, "Done. Parsed %d DNS packets" % len(dnsPackets)
+    print >> sys.stderr, "Done. Parsed %d DNS packets" % len(dnsPackets)
+
--- src/pcap_parser.py
+++ src/pcap_parser.py
@@ -5,66 +5,66 @@ import ipaddress
 import socket
 
 class ResourceRecord(object):
-  def __init__(self, ip, rr):
-    self.ip = ip
-    self.rr = rr
-    self.srcAddress = socket.inet_ntoa(ip.src)
-    self.dstAddress = socket.inet_ntoa(ip.dst)
-    self.rrTargetAddress = ipaddress.IPv4Address(self.dns.an[0].rdata)
+    def __init__(self, ip, rr):
+        self.ip = ip
+        self.rr = rr
+        self.srcAddress = socket.inet_ntoa(ip.src)
+        self.dstAddress = socket.inet_ntoa(ip.dst)
+        self.rrTargetAddress = ipaddress.IPv4Address(self.dns.an[0].rdata)
 
 class Query(object):
-  def __init__(self, ip, query):
-    self.ip = ip
-    self.srcAddress = socket.inet_ntoa(ip.src)
-    self.dstAddress = socket.inet_ntoa(ip.dst)
-    self.query = query
-    self.name = query.name
+    def __init__(self, ip, query):
+        self.ip = ip
+        self.srcAddress = socket.inet_ntoa(ip.src)
+        self.dstAddress = socket.inet_ntoa(ip.dst)
+        self.query = query
+        self.name = query.name
 
 def parse(filename):
-  pcapFile = dpkt.pcap.Reader(open(filename,'r'))
+    pcapFile = dpkt.pcap.Reader(open(filename,'r'))
 
-  dnsPackets = []
-  for ts, pkt in pcapFile:
-    eth = dpkt.ethernet.Ethernet(pkt) 
-    packet = Packet(eth)
-    if packet.isDNS:
-      dnsPackets.append(packet)
+    dnsPackets = []
+    for ts, pkt in pcapFile:
+        eth = dpkt.ethernet.Ethernet(pkt) 
+        packet = Packet(eth)
+        if packet.isDNS:
+            dnsPackets.append(packet)
 
-  return dnsPackets
+    return dnsPackets
 
 class Packet(object):
-  def __init__(self, ethernetPacket):
-    self.ethernetPacket = ethernetPacket
-    self.query = None
-    self.records = []
-    self.isDNS = self.unpack()
-
-  def unpack(self, debug = False):
-    if self.ethernetPacket.type != dpkt.ethernet.ETH_TYPE_IP:
-      return False # DNS runs on top of IP
-
-    self.ip = self.ethernetPacket.data
-    if self.ip.p == dpkt.ip.IP_PROTO_UDP: # DNS runs over UDP
-      self.udp = self.ip.data
-
-      tb = None
-      try:
-        self.dns = dpkt.dns.DNS(self.udp.data)
-
-        if len(self.dns.an) == 0:
-          self.query = Query(self.ip, self.dns.qd[0])
-
+    def __init__(self, ethernetPacket):
+        self.ethernetPacket = ethernetPacket
+        self.query = None
         self.records = []
-        for rr in self.dns.an:
-          self.records.append(ResourceRecord(self.ip, rr))
-        return True
-      except Exception as e:
-        isDns = False
-        tb = traceback.format_exc()
-        if debug:
-          print >> sys.stderr, str(e)
-      finally:
-        if tb != None and debug:
-          print tb
-    else:
-      return False
+        self.isDNS = self.unpack()
+
+    def unpack(self, debug = False):
+        if self.ethernetPacket.type != dpkt.ethernet.ETH_TYPE_IP:
+            return False # DNS runs on top of IP
+
+        self.ip = self.ethernetPacket.data
+        if self.ip.p == dpkt.ip.IP_PROTO_UDP: # DNS runs over UDP
+            self.udp = self.ip.data
+
+            tb = None
+            try:
+                self.dns = dpkt.dns.DNS(self.udp.data)
+
+                if len(self.dns.an) == 0:
+                    self.query = Query(self.ip, self.dns.qd[0])
+
+                self.records = []
+                for rr in self.dns.an:
+                    self.records.append(ResourceRecord(self.ip, rr))
+                return True
+            except Exception as e:
+                isDns = False
+                tb = traceback.format_exc()
+                if debug:
+                    print >> sys.stderr, str(e)
+            finally:
+                if tb != None and debug:
+                    print tb
+        else:
+            return False
